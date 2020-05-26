06180ce2d34c38aafbac6d96cbd9799176a44241
BrokenShell <webmaster@sharpdesigndigital.com>
2020-05-25 20:29:24 -0700

    updates assignment 2

--- module2/titanic_db.py
+++ module2/titanic_db.py
@@ -1,17 +1,17 @@
 import psycopg2
 import pandas as pd
 from psycopg2.extras import execute_values
-
 from module2.creds import cred
 
 
 url = "https://github.com/BrokenShell/DS-Unit-3-Sprint-2-SQL-and-Databases/raw/master/module2-sql-for-analysis/titanic.csv"
+
 titanic = pd.read_csv(url).rename({
     "Siblings/Spouses Aboard": "SiblingsSpouses",
     "Parents/Children Aboard": "ParentsChildren",
 }, axis=1)
+
 titanic['Name'] = titanic["Name"].str.replace("'", "")
-print(titanic.head().to_string())
 
 cloud = psycopg2.connect(
     dbname=cred.dbname,
@@ -32,6 +32,7 @@ CREATE TABLE Titanic (
     ParentsChildren     INT,
     Fare                INT);
 """)
+
 # for row in titanic.values:
 #     cloud.cursor().execute(f"""
 #     INSERT INTO Titanic
