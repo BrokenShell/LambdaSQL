4109fd08c272831acf3bfeca5067e850a2b5fd8d
BrokenShell <webmaster@sharpdesigndigital.com>
2020-05-26 17:15:31 -0700

    updates assignment 2

--- module2/titanic_db.py
+++ module2/titanic_db.py
@@ -2,6 +2,8 @@ import psycopg2
 import pandas as pd
 from psycopg2.extras import execute_values
 from module2.creds import cred
+from psycopg2.extensions import register_adapter
+import numpy as np
 
 
 url = "https://github.com/BrokenShell/DS-Unit-3-Sprint-2-SQL-and-Databases/raw/master/module2-sql-for-analysis/titanic.csv"
@@ -21,39 +23,45 @@ cloud = psycopg2.connect(
 )
 cursor = cloud.cursor()
 
-cursor.execute('DROP TABLE IF EXISTS Titanic;')
-
 cursor.execute("""
+DROP TABLE IF EXISTS Titanic;
 CREATE TABLE Titanic (
-    Survived            INT,
-    Pclass              INT,
+    Survived            INT8,
+    Pclass              INT8,
     Name                varchar(120),
-    Sex                 varchar(10),
-    Age                 INT,
-    SiblingsSpouses     INT,
-    ParentsChildren     INT,
-    Fare                INT);
+    Gender              varchar(10),
+    Age                 INT8,
+    SiblingsSpouses     INT8,
+    ParentsChildren     INT8,
+    Fare                FLOAT8);
 """)
 
 # for row in titanic.values:
 #     cursor.execute(f"""
 #     INSERT INTO Titanic
-#     (Survived, Pclass, Name, Sex, Age, SiblingsSpouses, ParentsChildren, Fare)
+#     (Survived, Pclass, Name, Gender, Age, SiblingsSpouses, ParentsChildren, Fare)
 #     VALUES {tuple(row)};
 #     """)
 
 # for row in titanic.values:
 #     cursor.execute("""
 #     INSERT INTO Titanic
-#     (Survived, Pclass, Name, Sex, Age, SiblingsSpouses, ParentsChildren, Fare)
+#     (Survived, Pclass, Name, Gender, Age, SiblingsSpouses, ParentsChildren, Fare)
 #     VALUES %s;
 #     """, [tuple(row)])
 
 execute_values(cursor, """
     INSERT INTO Titanic
-    (Survived, Pclass, Name, Sex, Age, SiblingsSpouses, ParentsChildren, Fare)
+    (Survived, Pclass, Name, Gender, Age, SiblingsSpouses, ParentsChildren, Fare)
     VALUES %s;
 """, [tuple(row) for row in titanic.values])
+
+# register_adapter(np.int64, psycopg2._psycopg.AsIs)
+# execute_values(cursor, """
+#     INSERT INTO Titanic
+#     (Survived, Pclass, Name, Gender, Age, SiblingsSpouses, ParentsChildren, Fare)
+#     VALUES %s;
+# """, list(titanic.to_records(index=False)))
 
 cloud.commit()
 
